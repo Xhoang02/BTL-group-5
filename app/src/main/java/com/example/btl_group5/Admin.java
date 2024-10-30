331b281d355643ac649d1279fa8f134a6eff6b02
Xhoang02 <titus.xh02042004@gmail.com>
2024-10-30 20:33:20 +0700

    Update project with new changes

--- app/src/main/java/com/example/btl_group5/Admin.java
+++ app/src/main/java/com/example/btl_group5/Admin.java
@@ -12,35 +12,35 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Admin extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
-    BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin);
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView1 = findViewById(R.id.bottomNavigationView1);
+        bottomNavigationView1.setOnNavigationItemSelectedListener(this);
     }
-    Fragment_home fragmentHome = new Fragment_home();
-    Fragment_menu fragmentMenu = new Fragment_menu();
-    Fragment_shopping fragmentShopping = new Fragment_shopping();
-    Fragment_account fragmentAccount = new Fragment_account();
+    Fragment_revenue_admin revenue = new Fragment_revenue_admin();
+    Fragment_food_admin food = new Fragment_food_admin();
+    Fragment_shopping_admin shopping = new Fragment_shopping_admin();
+    Fragment_account_admin account = new Fragment_account_admin();
     //    @SuppressLint("NonConstantResourceId")
 //    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Lấy ID của item được chọn
         int selectedItemId = item.getItemId();
-        if (selectedItemId == R.id.home) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragmentHome).commit();
+        if (selectedItemId == R.id.revenue) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment1, revenue).commit();
             return true;
-        } else if (selectedItemId == R.id.menu) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragmentMenu).commit();
+        } else if (selectedItemId == R.id.food) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment1, food).commit();
             return true;
         } else if (selectedItemId == R.id.shoppingcart) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragmentShopping).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment1, shopping).commit();
             return true;
         } else if (selectedItemId == R.id.account) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragmentAccount).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment1, account).commit();
             return true;
         }
         return false;
---
+++ app/src/main/java/com/example/btl_group5/Fragment_account_admin.java
@@ -0,0 +1,15 @@
+package com.example.btl_group5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class Fragment_account_admin extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_account_admin, container, false);
+    }
+}
