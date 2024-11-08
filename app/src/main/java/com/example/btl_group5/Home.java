ccad3a2f337cde5d0c8e9f3cc49019e61a3ce591
Xhoang02 <titus.xh02042004@gmail.com>
2024-11-08 21:51:31 +0700

    Update Project

--- app/src/main/java/com/example/btl_group5/Home.java
+++ app/src/main/java/com/example/btl_group5/Home.java
@@ -1,36 +1,48 @@
 package com.example.btl_group5;
 
-import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity
-        implements BottomNavigationView.OnNavigationItemSelectedListener {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.util.ArrayList;
+
+public class Home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView bottomNavigationView;
+    SQLiteDatabase dtbAccount_mon;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        // Khởi tạo Adapter
+        //adapListView = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrMon);
+
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        // Khởi tạo cơ sở dữ liệu
+        dtbAccount_mon = openOrCreateDatabase("Food.db", MODE_PRIVATE, null);
     }
+
     Fragment_home fragmentHome = new Fragment_home();
     Fragment_menu fragmentMenu = new Fragment_menu();
     Fragment_shopping fragmentShopping = new Fragment_shopping();
     Fragment_account fragmentAccount = new Fragment_account();
-//    @SuppressLint("NonConstantResourceId")
-//    @Override
+
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Lấy ID của item được chọn
         int selectedItemId = item.getItemId();
         if (selectedItemId == R.id.home) {
             getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragmentHome).commit();
@@ -43,11 +55,10 @@ public class Home extends AppCompatActivity
             return true;
         } else if (selectedItemId == R.id.account) {
             getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragmentAccount).commit();
-        return true;
+            return true;
         }
-        return false; // Trả về false nếu không có item nào được chọn
+        return false;
     }
-    //Client click vào món khai vị
     public void Khaivi(View view) {
         AlertDialog.Builder ad = new AlertDialog.Builder(Home.this);
         LayoutInflater inflater = Home.this.getLayoutInflater();
@@ -55,33 +66,7 @@ public class Home extends AppCompatActivity
         ad.setView(dialog);
         AlertDialog b = ad.create();
         b.show();
-    }
-    //Client click vào món chính
-    public void Monchinh(View view) {
-        AlertDialog.Builder ad = new AlertDialog.Builder(Home.this);
-        LayoutInflater inflater = Home.this.getLayoutInflater();
-        View dialog = inflater.inflate(R.layout.chinh_client, null);
-        ad.setView(dialog);
-        AlertDialog b = ad.create();
-        b.show();
-    }
-    //Client click vào món tráng miệng
-    public void Montrangmieng(View view) {
-        AlertDialog.Builder ad = new AlertDialog.Builder(Home.this);
-        LayoutInflater inflater = Home.this.getLayoutInflater();
-        View dialog = inflater.inflate(R.layout.trangmieng_client, null);
-        ad.setView(dialog);
-        AlertDialog b = ad.create();
-        b.show();
-    }
-    //Client click vào đồ uống
-    public void Douong(View view) {
-        AlertDialog.Builder ad = new AlertDialog.Builder(Home.this);
-        LayoutInflater inflater = Home.this.getLayoutInflater();
-        View dialog = inflater.inflate(R.layout.douong_client, null);
-        ad.setView(dialog);
-        AlertDialog b = ad.create();
-        b.show();
-    }
-}
 
+    }
+
+}
